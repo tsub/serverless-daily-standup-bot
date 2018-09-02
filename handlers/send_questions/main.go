c3534dabd73862976b181c174ff37474157f8a81
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2018-09-02 22:48:04 +0900

    feat: Add AuthorName and AuthorIcon in Slack Attachment

--- handlers/send_questions/main.go
+++ handlers/send_questions/main.go
@@ -8,13 +8,14 @@ import (
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/guregu/dynamo"
-	"github.com/lestrrat-go/slack"
 	"github.com/lestrrat-go/slack/objects"
 	"github.com/tsub/daily-standup-bot/lib/setting"
 	"github.com/tsub/daily-standup-bot/lib/standup"
+	"github.com/tsub/slack"
 )
 
 var slackToken = os.Getenv("SLACK_TOKEN")
+var botSlackToken = os.Getenv("SLACK_BOT_TOKEN")
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context, s setting.Setting) (setting.Setting, error) {
@@ -23,6 +24,7 @@ func Handler(ctx context.Context, s setting.Setting) (setting.Setting, error) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	botcl := slack.New(botSlackToken)
 	cl := slack.New(slackToken)
 
 	for _, userID := range s.UserIDs {
@@ -34,7 +36,7 @@ func Handler(ctx context.Context, s setting.Setting) (setting.Setting, error) {
 		if su.SentQuestionsCount == len(su.Answers) && len(su.Answers) != len(su.Questions) {
 			q := su.Questions[su.SentQuestionsCount]
 
-			resp, err := cl.Chat().PostMessage(userID).AsUser(true).Text(q).Do(ctx)
+			resp, err := botcl.Chat().PostMessage(userID).AsUser(true).Text(q).Do(ctx)
 			if err != nil {
 				return setting.Setting{}, err
 			}
@@ -45,18 +47,27 @@ func Handler(ctx context.Context, s setting.Setting) (setting.Setting, error) {
 		}
 
 		if len(su.Answers) == len(su.Questions) && !su.Finished {
-			message := cl.Chat().PostMessage(s.TargetChannelID).AsUser(true)
-
-			for i := range su.Questions {
-				attachment := &objects.Attachment{
-					Title: su.Questions[i],
-					Text:  su.Answers[i],
-				}
-
-				message.Attachment(attachment)
+			profile, err := cl.UsersProfile().Get().User(su.UserID).Do(ctx)
+			if err != nil {
+				return setting.Setting{}, err
 			}
 
-			resp, err := message.Do(ctx)
+			var fields objects.AttachmentFieldList
+			for i := range su.Questions {
+				fields.Append(&objects.AttachmentField{
+					Title: su.Questions[i],
+					Value: su.Answers[i],
+					Short: false,
+				})
+			}
+
+			attachment := &objects.Attachment{
+				AuthorName: profile.RealName,
+				AuthorIcon: profile.Image32,
+				Fields:     fields,
+			}
+
+			resp, err := botcl.Chat().PostMessage(s.TargetChannelID).AsUser(true).Attachment(attachment).Do(ctx)
 			if err != nil {
 				return setting.Setting{}, err
 			}
