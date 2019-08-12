8a8e32014f059e187c3b4a80288bd5ce634759c1
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2019-08-12 22:17:50 +0900

    wip: Write tests

--- internal/standup/standup_test.go
+++ internal/standup/standup_test.go
@@ -1,5 +1,75 @@
 package standup
 
-import "testing"
+import (
+	"fmt"
+	"testing"
 
-func TestAppendAnswerSuccess(*testing.T) {}
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/request"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/guregu/dynamo"
+)
+
+type mockedDynamo struct {
+	dynamodbiface.DynamoDBAPI
+	Resp *Standup
+}
+
+func (m *mockedDynamo) PutItemWithContext(context aws.Context, input *dynamodb.PutItemInput, options ...request.Option) (*dynamodb.PutItemOutput, error) {
+	for _, responseAnswer := range m.Resp.Answers {
+		for _, inputAnswer := range input.Item["answers"].SS {
+			if *inputAnswer != responseAnswer {
+				return nil,
+					fmt.Errorf("Mismatch answers, input: %q, response: %q",
+						*inputAnswer,
+						responseAnswer)
+			}
+		}
+	}
+
+	return &dynamodb.PutItemOutput{}, nil
+}
+
+func TestAppendAnswerSuccess(t *testing.T) {
+	want := &Standup{
+		UserID:  "user",
+		Answers: []string{"answer1", "answer2"},
+	}
+
+	standup := &Standup{
+		UserID:  "user",
+		Answers: []string{"answer1"},
+	}
+
+	mockedClient := &mockedDynamo{Resp: want}
+	db := dynamo.NewFromIface(mockedClient)
+
+	err := standup.AppendAnswer(db, "answer2")
+	if err != nil {
+		t.Fatalf("%q", err)
+	}
+}
+
+func TestCancelSuccess(t *testing.T) {
+	want := &Standup{
+		UserID:  "user",
+		Answers: []string{"none", "none"},
+	}
+
+	standup := &Standup{
+		UserID:  "user",
+		Answers: []string{"answer1"},
+	}
+
+	mockedClient := &mockedDynamo{Resp: want}
+	db := dynamo.NewFromIface(mockedClient)
+
+	err := standup.Cancel(db)
+	if err != nil {
+		t.Fatalf("%q", err)
+	}
+}
+
+func TestGetSuccess(t *testing.T)     {}
+func TestInitialSuccess(t *testing.T) {}
