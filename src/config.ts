aa4d2533e0b8368542139d5be1439418f3673507
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2019-09-07 23:26:05 +0900

    chore: Add required scope

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,7 @@ export const slackAuthorizationURL = "https://slack.com/oauth/authorize";
 export const slackTokenURL = "https://slack.com/api/oauth.access";
 export const slackAuthorizationScope = [
   "bot",
+  "commands",
   "chat:write:bot",
   "users.profile:read"
 ].join(" ");
